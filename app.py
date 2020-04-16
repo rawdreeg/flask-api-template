c409469c98b50d0dcb780c9e16578553e7e01bf6
Rodrigue Tusse <rodrigue.tusse@wundermanthompson.com>
2020-04-16 22:46:25 +0200

    fixes

--- app.py
+++ app.py
@@ -13,11 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key='YoRodrigue'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(ItemList, '/items')
