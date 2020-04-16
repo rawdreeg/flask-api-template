608d0acc79a8d483ea1432f1d73cbb08873b0b42
Rodrigue Tusse <rodrigue.tusse@wundermanthompson.com>
2020-04-16 22:34:12 +0200

    cleanup

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
 
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
