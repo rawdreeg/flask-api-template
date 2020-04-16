0e33f29a04f18b9282d95daf9ec441c992616ee4
Rodrigue Tusse <rodrigue.tusse@wundermanthompson.com>
2020-04-16 22:23:19 +0200

    set up for deployment

--- models/user.py
+++ models/user.py
@@ -1,4 +1,3 @@
-import sqlite3
 from db import db
 
 class UserModel(db.Model):
--- resources/item.py
+++ resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
