d288969e32a2a310351b342f044d3498bae5a033
Rodrigue Tusse <rodrigue.tusse@wundermanthompson.com>
2020-04-16 22:33:38 +0200

    move import

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from db import db
+
 
 
 app = Flask(__name__)
@@ -27,6 +29,5 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(UserRegister, '/signup')
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
     app.run(port=9292, debug=True)
